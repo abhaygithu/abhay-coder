8bbc9c953d734a47172c20bdf9fcfc0157966aa8
iCoders <91646339+abhaygithu@users.noreply.github.com>
2021-10-22 13:36:26 +0530

    Add files via upload

---
+++ alphabet.cxx
@@ -0,0 +1,91 @@
+#include<stdio.h>
+
+int main(){
+	
+	printf( " enter letter");
+	char c;
+	scanf( "%c",&c);
+	printf( " %c\n",c);
+	if ( c=='a'||c=='A'){
+		printf( " Apple");
+	}
+	else if ( c=='b'||c=='B'){
+		printf( " ball");
+	}
+		else if ( c=='c'||c=='C'){
+		printf( " cat");
+	}
+		else if ( c=='d'||c=='D'){
+		printf( " doll");
+	}
+		else if ( c=='e'||c=='E'){
+		printf( " elephant");
+	}
+	else if ( c=='f'||c=='F'){
+		printf( " fan");
+	}
+		else if ( c=='g'||c=='G'){
+		printf( " girl");
+	}
+		else if ( c=='h'||c=='H'){
+		printf( " hen");
+	}
+		else if ( c=='i'||c=='I'){
+		printf( "Ice cream");
+	}
+		else if ( c=='j'||c=='J'){
+		printf( "joy");
+	}
+		else if ( c=='k'||c=='K'){
+		printf( "kite");
+	}
+		else if ( c=='l'||c=='L'){
+		printf( " lion");
+	}
+		else if ( c=='m'||c=='M'){
+		printf( "monkey");
+	}
+		else if ( c=='n'||c=='N'){
+		printf( " net");
+	}
+		else if ( c=='o'||c=='O'){
+		printf( " owl");
+	}
+		else if ( c=='p'||c=='P'){
+		printf( " pen");
+	}
+		else if ( c=='q'||c=='Q'){
+		printf( " queen");
+	}
+		else if ( c=='r'||c=='R'){
+		printf( " rat");
+	}
+		else if ( c=='s'||c=='S'){
+		printf( " sun");
+	}
+		else if ( c=='t'||c=='T'){
+		printf( " tap");
+	}
+		else if ( c=='u'||c=='U'){
+		printf( "umbrella");
+	}
+		else if ( c=='v'||c=='V'){
+		printf( " van");
+	}
+		else if ( c=='w'||c=='W'){
+		printf( "window");
+	}
+		else if ( c=='x'||c=='X'){
+		printf( "x mas");
+	}
+		else if ( c=='y'||c=='Y'){
+		printf( "yak");
+	}
+		else if ( c=='z'||c=='Z'){
+		printf( " zebra");
+	}
+	else{
+		printf( "invalid entry plz try again");
+	}
+	
+}
